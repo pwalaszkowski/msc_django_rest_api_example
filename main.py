96dbd32a2b50a521d32821390ba39c96a90f97a5
Patryk Walaszkowski 80744 <patryk.walaszkowski@gmail.com>
2024-10-05 13:02:57 +0200

    linter_issues

--- main.py
+++ main.py
@@ -2,4 +2,3 @@ import django
 
 if __name__ == '__main__':
     print(django.get_version())
-
--- oAuthApp/exampleAPIApp/apps.py
+++ oAuthApp/exampleAPIApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ExampleapiappConfig(AppConfig):
+class ExampleApiAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'exampleAPIApp'
