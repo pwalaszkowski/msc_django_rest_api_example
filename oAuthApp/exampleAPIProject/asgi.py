0629208dc40da5b9ba23eebe0721e99e203133ee
Patryk Walaszkowski 80744 <patryk.walaszkowski@gmail.com>
2024-10-05 12:01:55 +0200

    django_setup_project_and_app

--- oAuthApp/exampleAPIProject/asgi.py
+++ oAuthApp/exampleAPIProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for oAuthApp project.
+ASGI config for exampleAPIProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oAuthApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exampleAPIProject.settings')
 
 application = get_asgi_application()
